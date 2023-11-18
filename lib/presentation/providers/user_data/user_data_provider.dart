d02c1a69421bb1c519e8b9f7b2b5553192e65ae2
mjohanarif <m.johanarifin@gmail.com>
2023-11-18 16:39:15 +0700

    Implement user data provider

---
+++ lib/presentation/providers/user_data/user_data_provider.dart
@@ -0,0 +1,138 @@
+import 'package:flix_id/domain/entities/entities.dart';
+import 'package:flix_id/domain/usecase/get_logged_in_user/get_logged_in_user.dart';
+import 'package:flix_id/domain/usecase/login/login.dart';
+import 'package:flix_id/domain/usecase/logout/logout.dart';
+import 'package:flix_id/domain/usecase/register/register.dart';
+import 'package:flix_id/domain/usecase/register/register_param.dart';
+import 'package:flix_id/domain/usecase/top_up/top_up.dart';
+import 'package:flix_id/domain/usecase/top_up/top_up_param.dart';
+import 'package:flix_id/domain/usecase/upload_profile_picture/upload_profile_picture.dart';
+import 'package:flix_id/domain/usecase/upload_profile_picture/upload_profile_picture_param.dart';
+
+import 'package:flix_id/presentation/providers/movie/now_playing_provider.dart';
+import 'package:flix_id/presentation/providers/movie/upcoming_provider.dart';
+import 'package:flix_id/presentation/providers/usecase/get_logged_in_user_repository.dart';
+import 'package:flix_id/presentation/providers/usecase/login_provider.dart';
+import 'package:flix_id/presentation/providers/usecase/logout_provider.dart';
+import 'package:flix_id/presentation/providers/usecase/register_provider.dart';
+import 'package:flix_id/presentation/providers/usecase/top_up_provider.dart';
+import 'package:flix_id/presentation/providers/usecase/upload_profile_picture_provider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_data_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+class UserDataProvider extends _$UserDataProvider {
+  @override
+  Future<User?> build() async {
+    GetLoggedInUser getLoggedInUser = ref.read(getLoggedInUserProvider);
+
+    final result = await getLoggedInUser(null);
+    switch (result) {
+      case Success(value: final user):
+        _getMovies();
+        return user;
+      case Failed():
+        return null;
+    }
+  }
+
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    state = const AsyncLoading();
+    Login login = ref.read(loginProvider);
+    final result = await login(
+      LoginParams(email: email, password: password),
+    );
+
+    switch (result) {
+      case Success(value: final user):
+        _getMovies();
+        state = AsyncData(user);
+      case Failed(:final message):
+        state = AsyncError(FlutterError(message), StackTrace.current);
+        state = const AsyncData(null);
+    }
+  }
+
+  Future<void> register({
+    required RegisterParam param,
+  }) async {
+    state = const AsyncLoading();
+    Register register = ref.read(registerProvider);
+    final result = await register(
+      RegisterParam(
+        name: param.name,
+        email: param.email,
+        password: param.password,
+      ),
+    );
+
+    switch (result) {
+      case Success(value: final user):
+        _getMovies();
+        state = AsyncData(user);
+      case Failed(:final message):
+        state = AsyncError(FlutterError(message), StackTrace.current);
+        state = const AsyncData(null);
+    }
+  }
+
+  Future<void> refreshUserData() async {
+    GetLoggedInUser getLoggedInUser = ref.read(getLoggedInUserProvider);
+
+    final result = await getLoggedInUser(null);
+    if (result is Success) {
+      state = AsyncData(result.resultValue);
+    }
+  }
+
+  Future<void> logOut() async {
+    Logout logout = ref.read(logoutProvider);
+    final result = await logout(null);
+    switch (result) {
+      case Success():
+        state = const AsyncData(null);
+      case Failed(:final message):
+        state = AsyncError(FlutterError(message), StackTrace.current);
+        state = AsyncData(state.valueOrNull);
+    }
+  }
+
+  Future<void> topUp({required int amount}) async {
+    TopUp topUp = ref.read(topUpProvider);
+    final userId = state.valueOrNull?.uid;
+
+    if (userId != null) {
+      final result = await topUp(
+        TopUpParam(amount: amount, userId: userId),
+      );
+
+      if (result.isSuccess) {
+        refreshUserData();
+        // TODO Refresh Transaction data
+      }
+    }
+  }
+
+  Future<void> uploadProfilePicture({
+    required UploadProfilePictureParam param,
+  }) async {
+    UploadProfilePicture uploadProfilePicture =
+        ref.read(uploadProfilePictureProvider);
+
+    final result = await uploadProfilePicture(param);
+
+    if (result case Success(value: final user)) {
+      state = AsyncData(user);
+    }
+  }
+
+  void _getMovies() {
+    ref.read(nowPlayingProvider.notifier).getMovies();
+    ref.read(upcomingProvider.notifier).getMovies();
+  }
+}
---
+++ lib/presentation/providers/user_data/user_data_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_data_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userDataProviderHash() => r'7bfb486ff0fe1ed525a557c6d995baa862e43e7c';
+
+/// See also [UserDataProvider].
+@ProviderFor(UserDataProvider)
+final userDataProviderProvider =
+    AsyncNotifierProvider<UserDataProvider, User?>.internal(
+  UserDataProvider.new,
+  name: r'userDataProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userDataProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserDataProvider = AsyncNotifier<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
