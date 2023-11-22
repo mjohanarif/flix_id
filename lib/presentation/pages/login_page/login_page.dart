4a9532fce06fbb0c713698d8880ea7e5919f29b5
mjohanarif <m.johanarifin@gmail.com>
2023-11-22 20:04:26 +0700

    Implement design login page

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flix_id/presentation/extensions/build_context_extension.dart';
+import 'package:flix_id/presentation/misc/methods.dart';
 import 'package:flix_id/presentation/providers/router/router_provider.dart';
 import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flix_id/presentation/widgets/flix_text_field.dart';
@@ -6,7 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginPage extends ConsumerWidget {
-  final TextEditingController textController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
   LoginPage({super.key});
 
   @override
@@ -21,30 +23,91 @@ class LoginPage extends ConsumerWidget {
       }
     });
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        child: Column(
-          children: [
-            FlixTextField(
-              labelText: 'Email',
-              controller: textController,
+      body: ListView(
+        children: [
+          verticalSpaces(100),
+          Center(
+            child: Image.asset(
+              'assets/flix_logo.png',
+              width: 150,
             ),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(userDataProvider.notifier).login(
-                      email: 'arifinjohan493@gmail.com',
-                      password: 'johanarif',
-                    );
-              },
-              child: const Text('Login'),
+          ),
+          verticalSpaces(100),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-          ],
-        ),
+            child: Column(
+              children: [
+                FlixTextField(
+                  labelText: 'Email',
+                  controller: emailController,
+                ),
+                verticalSpaces(24),
+                FlixTextField(
+                  labelText: 'Password',
+                  controller: passController,
+                  obscureText: true,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot password?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                verticalSpaces(24),
+                switch (ref.watch(userDataProvider)) {
+                  AsyncData(:final value) => value == null
+                      ? SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              ref.read(userDataProvider.notifier).login(
+                                    email: emailController.text,
+                                    password: passController.text,
+                                  );
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        )
+                      : const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                  _ => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                },
+                verticalSpaces(24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't hace an account? "),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Register here',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
